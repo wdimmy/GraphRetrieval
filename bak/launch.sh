12b846e05fb0e881feb604ba3e3a104a229342d8
Dingmin Wang <897246277@qq.com>
2023-01-14 20:29:04 +0000

    Delete bak directory

--- bak/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run --rm -it \
-  --runtime=nvidia \
-  --shm-size=1g \
-  --ulimit memlock=-1 \
-  --ulimit stack=67108864 \
-  -e NVIDIA_VISIBLE_DEVICES=$1 \
-  -v ${PWD}:/home/qiliu/pc2d \
-  pc2d bash
